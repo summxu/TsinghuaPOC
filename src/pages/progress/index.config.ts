915992c42e80d0ef19305d8241a6255729d497b1
sisterop <1241793025@qq.com>
2023-01-13 15:36:06 +0800

    修改部分在IOS下为白色背景

--- src/pages/progress/index.config.ts
+++ src/pages/progress/index.config.ts
@@ -1,9 +1,10 @@
 /*
  * @Author: Chenxu
  * @Date: 2022-12-28 13:26:25
- * @LastEditTime: 2023-01-13 09:05:28
+ * @LastEditTime: 2023-01-13 15:35:14
  * @Msg: Nothing
  */
 export default definePageConfig({
   navigationBarTitleText: '我的培养',
+    backgroundColor: '#FFFFFF'
 })
