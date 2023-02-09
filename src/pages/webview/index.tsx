8a6763c48bed2b0ae8b33501730982f0722fa117
sisterop <1241793025@qq.com>
2023-02-09 11:04:53 +0800

    修复webview

--- src/pages/webview/index.tsx
+++ src/pages/webview/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: Chenxu
  * @Date: 2022-12-28 13:26:25
- * @LastEditTime: 2023-02-09 10:32:16
+ * @LastEditTime: 2023-02-09 11:03:15
  * @Msg: Nothing
  */
 import { View, WebView } from '@tarojs/components'
@@ -13,10 +13,10 @@ import './index.scss'
 const WebViewPage: FC = () => {
   const [url, setUrl] = useState('')
   const { params } = useRouter()
-  useEffect(() => { setUrl(params.url!) }, [params])
+  useEffect(() => { setUrl(decodeURIComponent(params.url!)) }, [params])
   return (
     <View className='webview-box'>
-      <WebView src={url} />
+      <WebView className='webview' src={url} />
     </View>
   )
 }
