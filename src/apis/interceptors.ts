e4fb3bac78941005ec10d26c5e90a3f579654b03
sisterop <1241793025@qq.com>
2023-02-16 15:16:07 +0800

    修改刚进入提示

--- src/apis/interceptors.ts
+++ src/apis/interceptors.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Chenxu
  * @Date: 2022-12-29 10:43:58
- * @LastEditTime: 2023-02-07 14:09:05
+ * @LastEditTime: 2023-02-16 15:15:51
  * @Msg: Nothing
  */
 import Taro, { Chain } from "@tarojs/taro"
@@ -75,7 +75,7 @@ const customInterceptor = (chain: Chain) => {
       }
     })
     .catch(error => {
-      if (error === 'Authorization verification failed') {
+      if (error === 'Authorization verification failed' || error === '用户不存在') {
         return Promise.reject(error)
       }
       Taro.showToast({
